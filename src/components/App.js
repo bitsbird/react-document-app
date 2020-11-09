b2e0194b29ea8395870d51d7858eb714f76fe3a8
e.covello <emanuele.covello@gmail.com>
2020-11-09 17:21:10 +0100

    app setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import style from './App.module.scss';
+
+const Header = () => {
+  return <div>header</div>;
+};
+
+const Footer = () => {
+  return <div>footer</div>;
+};
+
+const Main = () => {
+  return <div>main</div>;
+};
 
 const App = () => {
-  return <div>Here is my App</div>;
+  return (
+    <div className={style.App}>
+      <div className={style.header}>
+        <Header />
+      </div>
+      <div className={style.scrollable}>
+        <main className={style.mainContent}>
+          <Main />
+        </main>
+        <div className={style.footer}>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
